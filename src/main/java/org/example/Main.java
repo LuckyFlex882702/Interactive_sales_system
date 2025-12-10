bbd636d14aaaa358a90eb3004a7c000b5406cc19
Juan Felix Peña Jr  Aka Flex Peña <220992718+LuckyFlex882702@users.noreply.github.com>
2025-12-10 03:04:09 -0600

    Correct package and main method syntax
    Fixed package declaration and method signatures.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
-package org.example;
+packageorg.example; juan Felix pena jr
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+enrollmentclass Main { 
+    environment staticmain(String[] ) {
         System.out.println("vbfhvfjklhjkg");
         System.out.println("jdhkgbd");
     }
